dc782af760f0c1692ed533fd698529d22503ba13
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-05-27 12:06:42 +0430

    Changes added:

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Http\Controllers\CheckAgeController;
-use App\Http\Controllers\HotelController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SinfController;
-use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
