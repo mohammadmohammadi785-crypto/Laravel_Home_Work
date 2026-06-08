d2a0288a6595f87a79fba69ded30c47015089e70
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-08 08:26:27 +0430

    Changes added

--- app/Http/Requests/postRequest.php
+++ app/Http/Requests/postRequest.php
@@ -28,4 +28,12 @@ class postRequest extends FormRequest
             "image_url"=> "required|string|mimes:jpg,png,jpeg,gif,svg",
         ];
     }
+    public function message(){
+        return [
+            "title.required" => "شما باید عنوان پست را مشخص کنید",
+            "title.string"=> "شما باید عنوان پست را متن اضافه نمایید",
+            "title.min"=> "شما حداقل باید 5 حرف برای عنوان اضافه نمایید",
+            "image_url.mimes"=> "پسوند فایل عکس باید به فرمت jpg, png, jpeg, gif, svg باشد"
+        ];
+    }
 }
