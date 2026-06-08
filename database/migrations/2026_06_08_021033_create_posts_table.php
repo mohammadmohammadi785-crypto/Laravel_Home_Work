824f0548229cdf771eb403df85b9a2a5e679af69
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-08 06:43:07 +0430

    Changes added

--- database/migrations/2026_06_08_021033_create_posts_table.php
+++ database/migrations/2026_06_08_021033_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->softDeletes();
+            $table->string('title');
+            $table->string('image_url');
             $table->timestamps();
         });
     }
