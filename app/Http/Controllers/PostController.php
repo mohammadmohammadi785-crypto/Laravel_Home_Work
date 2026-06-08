c4828bdc329728e36c2c008bbf9e6bf91c9b16a5
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-08 06:51:55 +0430

    Changes added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     //
+    public function index(){
+        $data = DB::table('posts')->get();
+    }
+
+    public function store(){
+
+    }
+
+    public function show(String $id){
+
+    }
+
+    public function edit(String $id){
+
+    }
+
+    public function update(){
+
+    }
 }
