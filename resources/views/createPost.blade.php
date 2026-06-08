8e498a7f978e953a94c16d1220b5a5b9c532475c
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-08 08:41:12 +0430

    Changes added

---
+++ resources/views/createPost.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class='w-full h-screen flex justify-center items-center'>
+        <form action="" method="post">
+            <input name="title" type="text" placeholder="Enter the Title">
+            <input name="image_url" type="file" accept="image/*">
+        </form>
+    </div>
+</body>
+</html>
