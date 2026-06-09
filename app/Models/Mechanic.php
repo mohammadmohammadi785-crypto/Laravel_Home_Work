62e4e15f1b8c9185043c5f53bb13f7d271b132af
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-09 08:37:29 +0430

    Changes added

--- app/Models/Mechanic.php
+++ app/Models/Mechanic.php
@@ -9,4 +9,10 @@ class Mechanic extends Model
 {
     /** @use HasFactory<\Database\Factories\MechanicFactory> */
     use HasFactory;
+    protected $fillable = [
+        'name',
+    ];
+    public function car(){
+        return $this->hasOne(Mechanic::class);
+    }
 }
