d796bedb12b899d6f8d4629731cfe4e2b4200c29
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-12 07:39:06 +0430

    Changes added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\MechanicController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,7 @@ Route::post("create-p", [PostController::class,"store"]);
 Route::get("mechanic", [MechanicController::class, "index"]);
 
 Route::get("Country", [CountryController::class, "index"]);
+
+Route::view('/product/create', 'Product.create');
+Route::post('/product/add', [ProductController::class, 'create']);
+Route::get('product', [ProductController::class, 'index']);
