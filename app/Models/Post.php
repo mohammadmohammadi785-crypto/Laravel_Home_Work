fe565cc3ab0cb480602ed3d354550ee02d82fa11
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-13 08:39:42 +0430

    Changes added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Image;
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\PostFactory> */
-    use HasFactory;
+    protected $fillable = [
+        "title",
+        "body"
+    ];
+    public function images(){
+        return $this->morphMany(Image::class, "imageable");
+    }
 }
