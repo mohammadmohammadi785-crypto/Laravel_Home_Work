c333b198d8ca4bde30c6e216dfdad75c90ad713e
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-15 08:03:24 +0430

    Changes added

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,3 @@ Route::get("Country", [CountryController::class, "index"]);
 Route::view('/product/create', 'Product.create');
 Route::post('/product/add', [ProductController::class, 'create']);
 Route::get('product', [ProductController::class, 'index']);
-
-Route::post("create-post",[PostController::class, "create"]);
-Route::get("Post",[PostController::class, "index"]);
-Route::view("Post/create", 'Post.create');
-Route::put('Post/update/{id}', [PostController::class, 'update']);
-Route::get('Post/edit/{id}', [PostController::class, 'edit']);
